73f75e6ceb200674e4a019bf73ff9815e7751e05
Alphacharge <rbetz@2-G-6.42heilbronn.de>
2022-11-14 09:48:36 +0100

    splittet functions in 2 files

--- src/environment/environment.c
+++ src/environment/environment.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_env.c                                          :+:      :+:    :+:   */
+/*   environment.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/19 10:41:28 by rbetz             #+#    #+#             */
-/*   Updated: 2022/11/11 17:27:24 by rbetz            ###   ########.fr       */
+/*   Created: 2022/11/14 09:47:16 by rbetz             #+#    #+#             */
+/*   Updated: 2022/11/14 09:47:30 by rbetz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,49 +112,6 @@ t_env	*extract_env(char **envp)
 		}
 	}
 	return (env);
-}
-/*transfers the PATH variable in an double ** array*/
-char	**get_path_var(t_env *env)
-{
-	int		i;
-	char	**ptr;
-
-	while (env != NULL)
-	{
-		if (ft_strncmp(env->name, "PATH", 4))
-		{
-			ptr = ft_split(env->value, ':');
-			break ;
-		}
-		env = env->next;
-	}
-	if (ptr == NULL)
-		return (NULL);
-	else
-		return (ptr);
-}
-
-/*searchs the path for a programm*/
-char	*get_path(char **paths, char *executable)
-{
-	int		i;
-	int		success;
-	char	*test;
-
-	i = 0;
-	success = -1;
-	while (paths != NULL && paths[i] != NULL && success != 0)
-	{
-		test = multijoin(false, 2, paths[i], executable);
-		success = access(test, F_OK);
-		if (success == -1)
-			free(test);
-		i++;
-	}
-	if (success == 0)
-		return (paths[--i]);
-	else
-		return (NULL);
 }
 
 // int	main(int argc, char **argv, char **envp)
