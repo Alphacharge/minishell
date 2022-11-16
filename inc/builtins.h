b20e9fd3ac747e5382f62d9075751ff393d3b768
Alphacharge <rbetz@student.42heilbronn.de>
2022-11-16 15:26:53 +0100

    added cd

--- inc/builtins.h
+++ inc/builtins.h
@@ -6,18 +6,19 @@
 /*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 09:39:46 by rbetz             #+#    #+#             */
-/*   Updated: 2022/11/14 12:42:28 by rbetz            ###   ########.fr       */
+/*   Updated: 2022/11/16 14:25:06 by rbetz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-# include <stdio.h>
-# include "structs.h"
-# include "errorhandling.h"
+# include "environment.h"
 
-int	builtin_exit(t_cmd *cmd);
-int	builtin_cd(t_cmd *cmd);
+// int	builtin_exit(t_cmd *cmd);
+// int	builtin_cd(t_cmd *cmd);
 
+void	echo(int argc, char **argv);
+void	env(int argc, char **argv, t_env *env);
+void	pwd(int argc, char **argv);
 #endif
---
+++ src/builtins/cd.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 14:23:11 by rbetz             #+#    #+#             */
+/*   Updated: 2022/11/16 15:23:44 by rbetz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <dirent.h>
+#include "environment.h"
+#include "libft.h"
+
+void	update_pwd(char *var, t_env *env)
+{
+	t_env	*tmp;
+	char	*old;
+
+	tmp = env;
+	while (tmp != NULL && !ft_strncmp(tmp->name, "PWD", 3))
+		tmp = tmp->next;
+	old = tmp->value;
+	tmp = env;
+	while (tmp != NULL && !ft_strncmp(tmp->name, "OLDPWD", 6))
+		tmp = tmp->next;
+	free(tmp->value);
+	tmp->value = old;
+	tmp = env;
+	while (tmp != NULL && !ft_strncmp(tmp->name, "PWD", 3))
+		tmp = tmp->next;
+	tmp->value = multijoin(true, 2, var, tmp->value);
+}
+
+void	cd(int argc, char **argv, t_env *env)
+{
+	int	ret;
+
+	ret = 0;
+	if (argc == 1)
+		ret = chdir("~");
+	else if (argc == 2)
+	{
+		if (argv[1] != NULL && ft_strlen(argv[1]) > 0)
+		ret = chdir(argv[1]);
+	}
+	else
+	{
+		write(2, "cd: string not in pwd: ", 23);
+		if (argv[1] != NULL)
+			write(2, argv[1], ft_strlen(argv[1]));
+		write(2, '\n', 1);
+	}
+	if (ret == -1)
+	{
+		write(2, "cd: no such file or directory: ", 30);
+		if (argv[1] != NULL)
+			write(2, argv[1], ft_strlen(argv[1]));
+		write(2, '\n', 1);
+	}
+	else
+		update_pwd(argv[1], env);
+}
