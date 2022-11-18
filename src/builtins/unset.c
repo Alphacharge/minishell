f7faca3dfb3483d9ef5d81788ece46f71ebcde8e
Alphacharge <rbetz@student.42heilbronn.de>
2022-11-18 14:19:58 +0100

    exit, export, unset

---
+++ src/builtins/unset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 10:52:36 by rbetz             #+#    #+#             */
+/*   Updated: 2022/11/18 13:59:43 by rbetz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "environment.h"
+#include "libft.h"
+
+/*accepts var to unset in env*/
+t_env	*unset(char *var, t_env *env)
+{
+	t_env	*head;
+	t_env	*tmp;
+	int		len;
+	
+	if (var == NULL || env == NULL)
+		return (NULL);
+	len = ft_strlen(var);
+	if (!ft_strncmp(var, env->name, len))
+		head = 
+}
