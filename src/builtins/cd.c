cbb2ccb912e30865360d381b107ebd7cfc8d39f2
Alphacharge <rbetz@student.42heilbronn.de>
2022-11-22 11:25:13 +0100

    created a new cd function

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:23:11 by rbetz             #+#    #+#             */
-/*   Updated: 2022/11/22 10:32:54 by rbetz            ###   ########.fr       */
+/*   Updated: 2022/11/22 11:20:33 by rbetz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,44 @@
 #include "builtins.h"
 #include "errorhandling.h"
 
-static void	update_pwd(char *var, t_env *env)
+// static void	update_pwd(char *var, t_env *env)
+// {
+// 	t_env	*tmp;
+// 	char	*old;
+// // needs to be changed :D function from env and export
+// 	tmp = env;
+// 	while (tmp != NULL && ft_strcmp(tmp->name, "PWD"))
+// 		tmp = tmp->next;
+// 	old = tmp->value;
+// 	tmp = env;
+// 	while (tmp != NULL && ft_strcmp(tmp->name, "OLDPWD"))
+// 		tmp = tmp->next;
+// 	ft_free(tmp->value);
+// 	tmp->value = old;
+// 	tmp = env;
+// 	while (tmp != NULL && !ft_strcmp(tmp->name, "PWD"))
+// 		tmp = tmp->next;
+// 	//Invalid free is under this line
+// 	old = tmp->value;
+// 	//wrong absolut path
+// 	tmp->value = multijoin(false, 2, var, tmp->value);
+// 	old = ft_free(old);
+// }
+static t_env	*update_pwd(char *var, t_env *env)
 {
-	t_env	*tmp;
-	char	*old;
-// needs to be changed :D function from env and export
-	tmp = env;
-	while (tmp != NULL && ft_strcmp(tmp->name, "PWD"))
-		tmp = tmp->next;
-	old = tmp->value;
-	tmp = env;
-	while (tmp != NULL && ft_strcmp(tmp->name, "OLDPWD"))
-		tmp = tmp->next;
-	ft_free(tmp->value);
-	tmp->value = old;
-	tmp = env;
-	while (tmp != NULL && !ft_strcmp(tmp->name, "PWD"))
-		tmp = tmp->next;
-	//Invalid free is under this line
-	old = tmp->value;
-	//wrong absolut path
-	tmp->value = multijoin(false, 2, var, tmp->value);
-	old = ft_free(old);
+	char	*dir;
+
+	if (var != NULL && env != NULL)
+	{
+		dir = get_env_var(env, "PWD");
+		if (dir != NULL)
+			env = set_env_var(env, "OLDPWD", dir);
+		if (dir != NULL)
+			dir = getcwd(NULL, 0);
+		if (dir != NULL)
+			env = set_env_var(env, "PWD", dir);
+	}
+	return (env);
 }
 
 /*accepts 1 arg or no arg 4 home, needs env to change pwd*/
