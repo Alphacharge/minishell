de352cc860897880238e5a7821c6cca88f47a7c6
Alphacharge <rbetz@student.42heilbronn.de>
2023-01-28 14:15:56 +0100

    removed master from infilecheck, check for last input

--- src/redir/infile.c
+++ src/redir/infile.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   infile.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fkernbac <fkernbac@student.42.fr>          +#+  +:+       +#+        */
+/*   By: humbi <humbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 10:13:54 by rbetz             #+#    #+#             */
-/*   Updated: 2022/12/27 13:07:17 by fkernbac         ###   ########.fr       */
+/*   Updated: 2023/01/28 14:14:54 by humbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "redirects.h"
+
+/*check if the input is the last one*/
+static int last_input(t_redir *redir)
+{
+	if (redir == NULL)
+		return (0);
+	redir = redir->next;
+	while (redir != NULL)
+	{
+		if (redir->type == INPUT || redir->type == HERE)
+			return (0);
+		redir = redir->next;
+	}
+	return (1);
+}
 
 /*check for an other fd, these one should be from a infile, and close it*/
 /*then call itsself and create a new one*/
@@ -19,37 +34,44 @@ static void	check_infiles(t_cmd *cmd, t_redir *redir)
 	int	success;
 
 	success = 1;
-	if (access(redir->file, F_OK) != 0 || access(redir->file, R_OK) != 0)
+	if (access(redir->file, F_OK) != 0)
 	{
 		success = 0;
-		ft_error(NULL, redir->file, 1);
+		ft_error(NULL, NULL, "No such file or directory");
+	}
+	if (success == 1 && access(redir->file, R_OK) != 0)
+	{
+		success = 0;
+		ft_error(NULL, NULL, "Permission denied");
 	}
 	if (success == 0)
 	{
 		cmd->error = true;
 		cmd->rats[READ] = dup(STDIN);
 	}
-	if (cmd->here == false && success == 1)
+}
+
+/*close fd from heredoc if exist and read from infile*/
+static void	read_infile(t_cmd *cmd, t_redir *redir)
+{
+	if (cmd->rats[READ] == FD_UNUSED)
 	{
-		if (cmd->rats[READ] == FD_UNUSED)
+		cmd->rats[READ] = open(redir->file, O_RDONLY);
+		if (cmd->rats[READ] == -1)
 		{
-			cmd->rats[READ] = open(redir->file, O_RDONLY);
-			if (cmd->rats[READ] == -1)
-			{
-				ft_error(NULL, redir->file, 1);
-				cmd->error = true;
-			}
+			ft_error(NULL, NULL, "Error opening file");
+			cmd->error = true;
 		}
-		else
-		{
-			close_and_neg(&cmd->rats[READ]);
-			check_infiles(cmd, redir);
-		}
+	}
+	else
+	{
+		close_and_neg(&cmd->rats[READ]);
+		read_infile(cmd, redir);
 	}
 }
 
 /*goes through the cmd list and check 4 infiles*/
-/*if there is already a fd we have a heredoc and leave instantly*/
+/*if there is already a fd from here we check if it is the last input*/
 t_cmd	*handle_infiles(t_cmd *cmd)
 {
 	t_cmd	*tmp;
@@ -62,7 +84,11 @@ t_cmd	*handle_infiles(t_cmd *cmd)
 		while (tred != NULL)
 		{
 			if (tred->type == INPUT)
+			{
 				check_infiles(tmp, tred);
+				if (tmp->error == false && last_input(tred) == 1)
+					read_infile(tmp, tred);
+			}
 			tred = tred->next;
 		}
 		tmp = tmp->next;
